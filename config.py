75d1aaa352ec21b6f1b2cd2b43fdcc11ec826534
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2025-04-06 19:04:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,11 @@
 import os
 
-#Database 
-#Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hegodal811:rsRu17pspZAcp6V7@cluster0.prsvqax.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
-
-#Shortner (token system) 
-# check my discription to help by using my refer link of shareus.io
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "9f943360c339cec4fed66d9d5cbaa0c2b3d41f81")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "79a8621baf4ba2a396eed551eb5d89634d64998f")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/ultroid_official/18") # shareus ka tut_vid he 
